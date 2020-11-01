7905683aa81c66b54a9e8f6b76abc89369e86a42
Karina Fernandez <karina.fernandez@pedidosya.com>
2020-10-31 21:27:03 -0300

    Fix login - Validate user email and password

--- routes/usuario.js
+++ routes/usuario.js
@@ -4,6 +4,9 @@ const RestError = require('../rest-error');
 const Router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const conn = mongoose.connection;
+
 
 const usuarioSchema = require('../schemas/usuario');
 const Usuario = mongoose.model('Usuario', usuarioSchema);
@@ -11,7 +14,11 @@ const Usuario = mongoose.model('Usuario', usuarioSchema);
 // REGISTRO USUARIO
 Router.post('/registro', function (req, res, next) {
     let { nombre, apellido, email, contraseña, admin } = req.body;
-    let usuario = new Usuario({ nombre, apellido, email, contraseña: bcrypt.hashSync(contraseña, 10), admin });
+
+    let contra = req.body.contraseña;
+    contra = crypto.createHash('sha256').update(contra).digest('hex');
+
+    let usuario = new Usuario({ nombre, apellido, email, contraseña: contra, admin });
 
     usuario.save((err, usr) => {
         if (err) {
@@ -27,11 +34,7 @@ Router.post('/registro', function (req, res, next) {
                 next(new RestError(errors, 400));
             }
         } else {
-            usr.__v = undefined;
-            usr.contraseña = undefined;
-            res.json({
-                usuario: usr
-            });
+            res.json({ usuario: usr });
         }
     })
 });
@@ -39,39 +42,56 @@ Router.post('/registro', function (req, res, next) {
 // LOGIN
 Router.post('/login', function (req, res, next) {
     let body = req.body;
-    Usuario.findOne({ email: body.email }, (erro, usuarioDB) => {
-        if (erro) {
-            return res.status(500).json({
-                err: erro
-            })
-        }
-        // Verifica que exista un usuario con el mail escrita por el usuario.
-        if (!usuarioDB) {
-            return res.status(400).json({
-                err: {
-                    message: "Usuario o contraseña incorrectos"
-                }
-            })
-        }
-        // Valida que la contraseña escrita por el usuario, sea la almacenada en la db
-        if (!bcrypt.compare(body.contraseña, usuarioDB.contraseña)) {
-            return res.status(400).json({
-                err: {
-                    message: "Usuario o contraseña incorrectos"
-                }
-            });
-        }
-        // Genera el token de autenticación
-        let token = jwt.sign({
-            usuario: usuarioDB,
-        }, process.env.SEED_AUTENTICACION, {
-            expiresIn: process.env.CADUCIDAD_TOKEN
-        })
-        res.json({
-            usuario: usuarioDB,
-            token,
-        })
-    })
+    // Usuario.findOne({ email: body.email }, (erro, usr) => {
+    //     if (erro) {
+    //         return res.status(500).json({
+    //             err: erro
+    //         })
+    //     }
+    //     // Verifica que exista un usuario con el mail escrita por el usuario.
+    //     if (!usr) {
+    //         return res.status(400).json({
+    //             err: {
+    //                 message: "Usuario o contraseña incorrectos"
+    //             }
+    //         })
+    //     }
+    //     // Valida que la contraseña escrita por el usuario, sea la almacenada en la db
+    //     if (!bcrypt.compare(body.contraseña, usr.contraseña)) {
+    //         return res.status(400).json({
+    //             err: {
+    //                 message: "Usuario o contraseña incorrectos"
+    //             }
+    //         });
+    //     }
+    //     // Genera el token de autenticación
+    //     let token = jwt.sign({
+    //         usuario: usr,
+    //     }, process.env.SEED_AUTENTICACION, {
+    //         expiresIn: process.env.CADUCIDAD_TOKEN
+    //     })
+    //     res.json({
+    //         usuario: usr,
+    //         token,
+    //     })
+    // })
+    const email = req.body.email;
+    let  contraseña = req.body.contraseña;
+    contraseña = crypto.createHash('sha256').update(contraseña).digest('hex');
+
+    conn.db.collection('usuarios', function (err, collection) {
+        collection.findOne({email:email, contraseña:contraseña}, function(err, doc){
+            if(doc){
+           //     const token = jwt.sign(doc.usr, process.env.TOKEN_SECRET);
+                doc.contraseña = undefined;
+                // doc.token = token;
+                res.json({usr:doc});
+            }
+            else{
+                next(new RestError('Usuario o Contraseña invalidos', 401));
+            }
+        });
+    });
 });
 
 // OBTENER USUARIO por ID
