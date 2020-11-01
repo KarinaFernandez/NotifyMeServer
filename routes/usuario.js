3ec2d51387694ff634ca62d79dcb960642d54b00
Karina Fernandez <karina.fernandez@pedidosya.com>
2020-10-31 21:44:47 -0300

    User login refactor

--- routes/usuario.js
+++ routes/usuario.js
@@ -2,11 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const RestError = require('../rest-error');
 const Router = express.Router();
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
-const conn = mongoose.connection;
-
 
 const usuarioSchema = require('../schemas/usuario');
 const Usuario = mongoose.model('Usuario', usuarioSchema);
@@ -41,56 +38,16 @@ Router.post('/registro', function (req, res, next) {
 
 // LOGIN
 Router.post('/login', function (req, res, next) {
-    let body = req.body;
-    // Usuario.findOne({ email: body.email }, (erro, usr) => {
-    //     if (erro) {
-    //         return res.status(500).json({
-    //             err: erro
-    //         })
-    //     }
-    //     // Verifica que exista un usuario con el mail escrita por el usuario.
-    //     if (!usr) {
-    //         return res.status(400).json({
-    //             err: {
-    //                 message: "Usuario o contraseña incorrectos"
-    //             }
-    //         })
-    //     }
-    //     // Valida que la contraseña escrita por el usuario, sea la almacenada en la db
-    //     if (!bcrypt.compare(body.contraseña, usr.contraseña)) {
-    //         return res.status(400).json({
-    //             err: {
-    //                 message: "Usuario o contraseña incorrectos"
-    //             }
-    //         });
-    //     }
-    //     // Genera el token de autenticación
-    //     let token = jwt.sign({
-    //         usuario: usr,
-    //     }, process.env.SEED_AUTENTICACION, {
-    //         expiresIn: process.env.CADUCIDAD_TOKEN
-    //     })
-    //     res.json({
-    //         usuario: usr,
-    //         token,
-    //     })
-    // })
     const email = req.body.email;
-    let  contraseña = req.body.contraseña;
+    let contraseña = req.body.contraseña;
     contraseña = crypto.createHash('sha256').update(contraseña).digest('hex');
 
-    conn.db.collection('usuarios', function (err, collection) {
-        collection.findOne({email:email, contraseña:contraseña}, function(err, doc){
-            if(doc){
-           //     const token = jwt.sign(doc.usr, process.env.TOKEN_SECRET);
-                doc.contraseña = undefined;
-                // doc.token = token;
-                res.json({usr:doc});
-            }
-            else{
-                next(new RestError('Usuario o Contraseña invalidos', 401));
-            }
-        });
+    Usuario.findOne({ email: email, contraseña: contraseña }, function (err, doc) {
+        if (doc) {
+            res.json({ usr: doc });
+        } else {
+            next(new RestError('Usuario o Contraseña invalidos', 401));
+        }
     });
 });
 
