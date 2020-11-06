f598e74dd862b7cbb009cc3b4abc4044c7d3b088
Karina Fernandez <karina.fernandez@pedidosya.com>
2020-11-05 22:06:15 -0300

    Dot env added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const RestError = require('./rest-error');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 
 const usuario = require('./routes/usuario');
 const incidente = require('./routes/incidente');
 
-//require('dotenv').config(); 
+require('dotenv').config();
 
 // Conectar a DB local
 // const uri = 'mongodb://localhost:27017/incidentes';
@@ -14,31 +14,31 @@ const incidente = require('./routes/incidente');
 // mongoose.connect(uri,options); 
 
 const uri = process.env.MONGODB_URI;
-const options = {useNewUrlParser:true, useUnifiedTopology: true}; 
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
 //Evento
 mongoose.connect(uri, options).catch(error => {
     console.log('Hubo un error de conexión', error.message);
-}); 
+});
 
 const conn = mongoose.connection;
 //Error de conexion
 mongoose.connection.on('error', error => {
     console.log('Hubo un error de conexión', error.message);
-}); 
+});
 
 app.use(express.json());
 app.use(incidente);
 app.use(usuario);
 
-app.use((err,req,res,next) => {
-    res.status(err instanceof RestError? err.status: 500);
-    res.json({error:err.message});
+app.use((err, req, res, next) => {
+    res.status(err instanceof RestError ? err.status : 500);
+    res.json({ error: err.message });
 });
 
 
 process.env.CADUCIDAD_TOKEN = '48h';
-process.env.SEED_AUTENTICACION = process.env.SEED_AUTENTICACION ||  'este-es-el-seed-desarrollo';
+process.env.SEED_AUTENTICACION = process.env.SEED_AUTENTICACION || 'este-es-el-seed-desarrollo';
 
 /* Levanta Local
 app.listen(3000, function () {
@@ -46,6 +46,7 @@ app.listen(3000, function () {
 })
 */
 
-app.listen(process.env.PORT, function(){
-    console.log(`Escuchando puerto ${process.env.PORT}`);
-});
+app.listen(process.env.PORT, function () {
+    console.log(`Conectado a ${process.env.PORT}`
+    );
+}); 
