94c2066d5164b1287f296e860d22a0278e7bd48f
Karina Fernandez <karina.fernandez@pedidosya.com>
2020-11-08 18:44:19 -0300

    Populate incidents with user

--- routes/incidente.js
+++ routes/incidente.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 const RestError = require('../rest-error');
 const Router = express.Router();
 
-const incidenteSchema = require('../schemas/incidente');
-const incidente = mongoose.model('Incidente', incidenteSchema);
+const Incidente = mongoose.model('Incidente', require('../schemas/incidente'));
 
 // CREAR INCIDENTE
 Router.post('/incidentes', function (req, res, next) {
-    Inc = new incidente(req.body);
+    Inc = new Incidente(req.body);
 
     Inc.save(function (err, inc) {
         if (err) {
@@ -32,14 +31,31 @@ Router.post('/incidentes', function (req, res, next) {
 
 // OBTENER TODOS LOS INCIDENTES
 Router.get('/incidentes', function (req, res) {
-    incidente
+    Query = Incidente.find({}).populate('usuario');
+
+    Query.exec(function (err, incidentes) {
+        if (!err) {
+            res.json(incidentes);
+        } else {
+            errors = {};
+            for (const key in err.errors) {
+                if (err.errors[key].constructor.name != 'ValidationError') {
+                    errors[key] = err.errors[key].message;
+                }
+            }
+            next(new RestError(errors, 400));
+        }
+    })
+
+    /*
+    Incidente
         .find({})
         .populate('usuario')
         .exec((err, inc) => {
             if (err) return handleError(err);
             return inc
         });
-
+    */
 
     // incidente.find({}, (err, incidentes) =>
     //     res.send(incidentes.reduce((incidenteMap, inc) => {
@@ -53,7 +69,7 @@ Router.get('/incidentes', function (req, res) {
 // OBTENER INCIDENTE por ID
 Router.get('/incidentes/:id', function (req, res) {
     const id = req.params.id;
-    Query = incidente.findById(id)
+    Query = Incidente.findById(id)
     Query.exec(function (err, inc) {
         if (!err) {
             inc.__v = undefined;
@@ -65,7 +81,7 @@ Router.get('/incidentes/:id', function (req, res) {
 // ACTUALIZAR INCIDENTE (Validar / Deshabilitar)
 Router.put('/incidentes/:id', function (req, res, next) {
     const id = req.params.id;
-    incidente.findByIdAndUpdate(id, req.body, { new: true, runValidators: true }, function (err, incidente) {
+    Incidente.findByIdAndUpdate(id, req.body, { new: true, runValidators: true }, function (err, incidente) {
         if (!err) {
             if (incidente) {
                 res.json(incidente)
